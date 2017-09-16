f3e17353e56f3798a8ede0611465678ce9e24a48
Nabin Khadka <nbnkhadka14@gmail.com>
2017-09-16 23:16:55 +0545

    Add help comment

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-async_mode = None
 
+# Create a flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
+# Use the flask app to create a socketio decorator
+socketio = SocketIO(app)
 thread = None
 
 
@@ -14,6 +15,16 @@ def background_thread():
     while True:
         socketio.sleep(2)
         count += 1
+        """
+            On every 2 seconds, the server send message to the client with updated count.
+            First parameter of emit function tells which function to call on client side.
+            So the following part of index.html will be called.
+
+            socket.on('my_response', function(msg) {
+                $('span#view').text(msg.count);
+            });
+            
+        """
         socketio.emit('my_response',
                       {'data': 'Message from server', 'count': count},
                       namespace='/test')
@@ -21,13 +32,17 @@ def background_thread():
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    """
+        Home page view function
+    """
+    return render_template('index.html')
 
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     global thread
     if thread is None:
+        # Once any client is connected, the background_thread function starts in loop
         thread = socketio.start_background_task(target=background_thread)
     emit('my_response', {'data': 'Connected', 'count': 0})
 
